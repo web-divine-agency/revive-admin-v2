cc74d695521518a9ab39d590ab7c6637a0d5151e
Estropigan <rickyestropigan01281@gmail.com>
2024-10-14 14:52:19 +0800

    fixed nav text container height

--- src/components/SideBar/SideBarData.jsx
+++ src/components/SideBar/SideBarData.jsx
@@ -7,42 +7,44 @@ import branches from '../../assets/images/branches.png';
 import log_out from '../../assets/images/log_out.png';
 import generate_ticket from '../../assets/images/generate_ticket.png';
 import queue_list from '../../assets/images/queue_list.png';
+import "./SideBar.css";
+
 
 export const AdminSidebarData = [
   {
     title: 'Users List',
     path: '/userlist',
-    icon: <img src={users_list} alt="Custom Icon" style={{ width: '24px', height: '24px' }} />,
+    icon: <img className='navIcon' src={users_list} alt="Custom Icon" style={{ width: '24px', height: '24px' }} />,
     cName: 'nav-text'
   },
   {
     title: 'User Management',
     path: '/user-management',
-    icon: <img src={users_management} alt="Custom Icon" style={{ width: '24px', height: '24px' }} />,
+    icon: <img className='navIcon' src={users_management} alt="Custom Icon" style={{ width: '24px', height: '24px' }} />,
     cName: 'nav-text'
   },
   {
     title: 'Staff Logs',
     path: '/staff-logs',
-    icon: <img src={staff_logs} alt="Custom Icon" style={{ width: '24px', height: '24px' }} />,
+    icon: <img className='navIcon' src={staff_logs} alt="Custom Icon" style={{ width: '24px', height: '24px' }} />,
     cName: 'nav-text'
   },
   {
     title: 'Tickets History',
     path: '/tickets-history',
-    icon: <img src={tickets_history} alt="Custom Icon" style={{ width: '24px', height: '24px' }} />,
+    icon: <img className='navIcon' src={tickets_history} alt="Custom Icon" style={{ width: '24px', height: '24px' }} />,
     cName: 'nav-text'
   },
   {
     title: 'Branches',
     path: '/branches',
-    icon: <img src={branches} alt="Custom Icon" style={{ width: '24px', height: '24px' }} />,
+    icon: <img className='navIcon' src={branches} alt="Custom Icon" style={{ width: '24px', height: '24px' }} />,
     cName: 'nav-text'
   },
   {
     title: 'Logout',
     path: '/',
-    icon: <img src={log_out} alt="Custom Icon" style={{ width: '24px', height: '24px' }} />,
+    icon: <img className='navIcon' src={log_out} alt="Custom Icon" style={{ width: '24px', height: '24px' }} />,
     cName: 'nav-text',
     onClick: (event, logout) => logout(event)
   }
@@ -52,13 +54,13 @@ export const StaffSidebarData = [
   {
     title: 'Generate Ticket',
     path: '/generate-tickets',
-    icon: <img src={generate_ticket} alt="Custom Icon" style={{ width: '24px', height: '24px' }} />,
+    icon: <img className='navIcon' src={generate_ticket} alt="Custom Icon" style={{ width: '24px', height: '24px' }} />,
     cName: 'nav-text'
   },
   {
     title: 'History',
     path: '/history',
-    icon: <img src={tickets_history} alt="Custom Icon" style={{ width: '24px', height: '24px' }} />,
+    icon: <img className='navIcon' src={tickets_history} alt="Custom Icon" style={{ width: '24px', height: '24px' }} />,
     cName: 'nav-text'
   },
   // {
@@ -70,7 +72,7 @@ export const StaffSidebarData = [
   {
     title: 'Logout',
     path: '/',
-    icon: <img src={log_out} alt="Custom Icon" style={{ width: '24px', height: '24px' }} />,
+    icon: <img className='navIcon' src={log_out} alt="Custom Icon" style={{ width: '24px', height: '24px' }} />,
     cName: 'nav-text',
     onClick: (event, logout) => logout(event)
   }
