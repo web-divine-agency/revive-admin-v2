a660fd050191b7c3c210c95a5f2c2e422d24f3ff
tony-tripulca <tony.tripulca@gmail.com>
2025-02-08 02:44:15 +0800

    Deployed to uat

--- src/config/app.js
+++ src/config/app.js
@@ -8,12 +8,12 @@ const env = {
     resourceService: "http://localhost:8805",
   },
   uat: {
-    portal: "http://localhost:3001",
-    userService: "http://localhost:8801",
-    ticketService: "http://localhost:8802",
-    branchService: "http://localhost:8803",
-    loggerService: "http://localhost:8804",
-    resourceService: "http://localhost:8805",
+    portal: "http://nadescrib.com:3001",
+    userService: "http://nadescrib.com:8801",
+    ticketService: "http://nadescrib.com:8802",
+    branchService: "http://nadescrib.com:8803",
+    loggerService: "http://nadescrib.com:8804",
+    resourceService: "http://nadescrib.com:8805",
   },
 };
 
